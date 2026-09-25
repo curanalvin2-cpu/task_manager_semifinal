eafed8cc228703b3b04833a85bde19b49e0c0853
curanalvin2-cpu <curanalvin2@gmail.com>
2026-09-25 23:16:20 +0800

    Update Task.php

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'task_name',
+        'description',
+        'status',
+        'due_date',
+    ];
 }
