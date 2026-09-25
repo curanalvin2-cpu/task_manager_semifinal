8777f366fc02a5f6460dcbd81592564eaa2b756a
curanalvin2-cpu <curanalvin2@gmail.com>
2026-09-25 23:15:39 +0800

    Update 2026_09_25_145950_create_tasks_table.php

--- database/migrations/2026_09_25_145950_create_tasks_table.php
+++ database/migrations/2026_09_25_145950_create_tasks_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('task_name');
+            $table->text('description')->nullable();
+            $table->enum('status', ['Pending', 'Completed'])->default('Pending');
+            $table->date('due_date')->nullable();
             $table->timestamps();
         });
     }
